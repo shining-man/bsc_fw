ccf96b1b01db0b69426e7634d15fd6901b5830ff
Meik Jaeckle <meikjaeckle@gmail.com>
2024-01-21 18:34:02 +0000

    - Added Bitfield library - Added bitfield helper methods - Added JkBms bitfield definition for warning message - Added unit tests including platformio configuration - Updated c++ standard to c++17 and c standard to c11

---
+++ lib/bsc/types/Bitfields.hpp
@@ -0,0 +1,44 @@
+// Copyright (c) 2024 Meik Jäckle
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+
+#ifndef TYPES_BITFIELD_H
+#define TYPES_BITFIELD_H
+
+#include <jungles/bitfields.hpp>
+
+namespace types
+{
+
+namespace bf
+{
+
+using namespace jungles;
+
+template<class DATA_TYPE> //, typename std::enable_if<std::is_unsigned<DATA_TYPE>::value>::type>
+constexpr bool isBitIdxInRange(std::size_t bitIdx)
+{
+  return (bitIdx < std::numeric_limits<DATA_TYPE>::digits);
+}
+
+template<class DATA_TYPE> //, typename std::enable_if<std::is_unsigned<DATA_TYPE>::value>::type>
+constexpr auto bitIdxToValue(std::size_t bitIdx)
+{
+  static_assert(std::is_unsigned_v<DATA_TYPE>, "signed values are not supported yet");
+
+  if (isBitIdxInRange<DATA_TYPE>(bitIdx))
+  {
+      return (static_cast<DATA_TYPE>(1) << bitIdx);
+  }
+  else
+  {
+      assert(isBitIdxInRange<DATA_TYPE>(bitIdx) && "bitIdx is NOT in range");
+      return 0;
+  }
+}
+} // namespace bf
+} // namespace types
+
+#endif // TYPES_BITFIELD_H
